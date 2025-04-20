6ebf4e73d4e6fbd4586d1fcae79fc14fc8dbff60
cupcake1221 <sgopshtein@cchsctv.com>
2025-04-20 22:08:11 +0900

    Update hello.py
    Version 1.1.

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
-# Firstname Lastname
-# 11/15/2024
+# Sabrina Gopshtein
+# 04/20/2025
 # A simple Python program to print a message
+
+# Sabrina Gopshtein
+# 04/20/2025
+# added new message
+
 print("Hello, World!")
+
+print("Welcome to GitHub!")
